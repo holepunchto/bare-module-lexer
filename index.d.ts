90ab1e8a92d27d3d12b2b1a551bd557e1a8608e7
yasser <dev@yasser.email>
2025-01-06 17:26:34 -0300

    Add TypeScript typings (#1)

---
+++ index.d.ts
@@ -0,0 +1,36 @@
+import Buffer, { BufferEncoding } from 'bare-buffer'
+
+declare function lex(
+  input: string | Buffer,
+  encoding?: BufferEncoding
+): {
+  imports: {
+    specifier: string
+    type: number
+    names: string[]
+    position: [
+      importStart: number,
+      specifierStart: number,
+      specifierEnd: number
+    ]
+  }[]
+
+  exports: {
+    name: string
+    position: [exportStart: number, nameStart: number, nameEnd: number]
+  }[]
+}
+
+declare namespace lex {
+  export const constants: {
+    REQUIRE: number
+    IMPORT: number
+    DYNAMIC: number
+    ADDON: number
+    ASSET: number
+    RESOLVE: number
+    REEXPORT: number
+  }
+}
+
+export = lex
