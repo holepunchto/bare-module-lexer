e0e44cb84071a0c3a17a7d9de3fc2be1fc1b77dc
yasser <dev@yasser.email>
2025-02-17 09:57:51 -0300

    Export helper types (#4)

--- index.d.ts
+++ index.d.ts
@@ -1,27 +1,28 @@
 import Buffer, { BufferEncoding } from 'bare-buffer'
 
+interface Import {
+  specifier: string
+  type: number
+  names: string[]
+  position: [importStart: number, specifierStart: number, specifierEnd: number]
+}
+
+interface Export {
+  name: string
+  position: [exportStart: number, nameStart: number, nameEnd: number]
+}
+
 declare function lex(
   input: string | Buffer,
   encoding?: BufferEncoding
 ): {
-  imports: {
-    specifier: string
-    type: number
-    names: string[]
-    position: [
-      importStart: number,
-      specifierStart: number,
-      specifierEnd: number
-    ]
-  }[]
-
-  exports: {
-    name: string
-    position: [exportStart: number, nameStart: number, nameEnd: number]
-  }[]
+  imports: Import[]
+  exports: Export[]
 }
 
 declare namespace lex {
+  export { type Import, type Export }
+
   export const constants: {
     REQUIRE: number
     IMPORT: number
